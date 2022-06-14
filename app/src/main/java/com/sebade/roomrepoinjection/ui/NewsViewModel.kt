5d1949a56add56cebe13b669ed70bc2146a31ba2
ade bayu s <adebayus17@gmail.com>
2022-06-14 22:37:54 +0700

    with coroutine

--- app/src/main/java/com/sebade/roomrepoinjection/ui/NewsViewModel.kt
+++ app/src/main/java/com/sebade/roomrepoinjection/ui/NewsViewModel.kt
@@ -1,17 +1,25 @@
 package com.sebade.roomrepoinjection.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sebade.roomrepoinjection.data.NewsRepository
 import com.sebade.roomrepoinjection.data.local.entity.NewsEntity
+import kotlinx.coroutines.launch
 
 class NewsViewModel(private val newsRepository: NewsRepository) : ViewModel() {
     fun getHeadlineNews() = newsRepository.getHeadlineNews()
 
     fun getBookmarkedNews() = newsRepository.getBookmarkedNews()
+
     fun saveNews(news: NewsEntity) {
-        newsRepository.setBookmarkedNews(news, true)
+        viewModelScope.launch {
+            newsRepository.setNewsBookmark(news, true)
+        }
     }
+
     fun deleteNews(news: NewsEntity) {
-        newsRepository.setBookmarkedNews(news, false)
+        viewModelScope.launch {
+            newsRepository.setNewsBookmark(news, false)
+        }
     }
 }
